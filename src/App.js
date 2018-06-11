8ba7fc7ebc8c26cb2ae5057eb483bad646d010ea
seungwoohong <alwls601@naver.com>
2018-06-12 00:12:27 +0900

    연습중 ..

--- src/App.js
+++ src/App.js
@@ -2,28 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-];
-
-const moviePosters = [
-  "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
-  "https://ia.media-imdb.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg",
-  "https://ia.media-imdb.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg"
-];
-
 class App extends Component {
+
+  state = {
+    greeting: 'Hello',
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://ia.media-imdb.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+      },
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg",
+      },
+      {
+        title: "Star Wars",
+        poster: "https://ia.media-imdb.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg"
+      }
+    ]
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          ...this.state.movies,
+          {
+            title: 'Trainspotting',
+            poster: "https://ia.media-imdb.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg"
+           }
+        ]
+      })
+    }, 1000);
+  }
+
   render() {
+    var stat = this.state;
+
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} posterUrl={moviePosters[0]}/> 
-        <Movie title={movieTitles[1]} posterUrl={moviePosters[1]}/> 
-        <Movie title={movieTitles[2]} posterUrl={moviePosters[2]}/> 
-        <Movie title={movieTitles[3]} posterUrl={moviePosters[3]}/> 
+        {stat.greeting}
+        {stat.movies.map((movie, index) => {
+          return <Movie title={movie.title} posterUrl={movie.poster} key={index} />
+        })}
       </div>
     );
   }
--- src/movie.js
+++ src/movie.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './movie.css';
 
 class Movie extends Component {
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string
+    };
+
     render() {
         console.log(this.props);
         return (
